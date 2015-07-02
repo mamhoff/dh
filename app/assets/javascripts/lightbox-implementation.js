3f630ec55ba0f391f988671ded94213945d2171a
Marc Altmann <mail@marcaltmann.com>
2015-07-03 01:08:20 +0200

    Make Lightbox for sliders close on click

--- app/assets/javascripts/lightbox-implementation.js
+++ app/assets/javascripts/lightbox-implementation.js
@@ -1,6 +1,24 @@
 $(document).ready(function () {
-  $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
-  });
+  $(document)
+    .on('click', '*[data-toggle="lightbox"]', function (event) {
+      var $this = $(this);
+
+      // Establish two kinds of Lightboxes here - one for normal image galleries
+      // and one for sliders.
+      // Lightboxes for sliders are treated a little differently: They have no
+      // close button and are closed on click.
+      if ($this.data('always_show_close') === undefined) {
+        $(this).ekkoLightbox();
+      }
+      else {
+        $(this).ekkoLightbox({modal_id: 'lightbox_for_slider'});
+      }
+
+      event.preventDefault();
+    })
+
+    // Close Lightbox on click for Lightbox for sliders
+    .on('click', '#lightbox_for_slider', function (event) {
+      $(this).modal('hide');
+    });
 });
