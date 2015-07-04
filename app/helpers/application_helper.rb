14e8582c9f57f9b278d68827e72191f3af5b8605
Marc Altmann <mail@marcaltmann.com>
2015-07-04 23:17:14 +0200

    Move Vimeo logic into helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # Extract iframe src from Vimeo embed code.
+  def extract_vimeo_source (vimeo_embed_code)
+    vimeo_embed_code.match(/src=\"(.*?)\"/)[1].to_s
+  end
+
+  # Extract video id from Vimeo embed code.
+  def extract_vimeo_id (vimeo_embed_code)
+    extract_vimeo_source(vimeo_embed_code)
+      .match(/video\/(\d+)$/)[1].to_s
+  end
+
 end
