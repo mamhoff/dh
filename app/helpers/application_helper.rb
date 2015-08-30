4ae2e78a44a919afe76457d908e8596aebe21045
Martin Meyerhoff <mamhoff@gmail.com>
2015-08-30 17:58:11 +0200

    Display the question mark even if the image slider's empty

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,7 +27,7 @@ module ApplicationHelper
     image_slider = page.find_elements(only: ['image_slider',
       'image_gallery']).first
 
-    if image_slider
+    if image_slider && image_slider.contents.present?
       render_essence_view(image_slider.contents.gallery_pictures.first,
                           { image_size: '170x170', crop: true },
                           { class: 'dh-works-page-thumbnail' })
