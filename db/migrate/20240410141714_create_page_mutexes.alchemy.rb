032b581040cd06cda78f39e4f9f1dc5b82fb34e7
Martin Meyerhoff <mamhoff@gmail.com>
2024-04-11 19:03:30 +0200

    Oops migration?

--- db/migrate/20240410141714_create_page_mutexes.alchemy.rb
+++ db/migrate/20240410141714_create_page_mutexes.alchemy.rb
@@ -1,9 +1,11 @@
 # This migration comes from alchemy (originally 20231113104432)
 class CreatePageMutexes < ActiveRecord::Migration[6.1]
   def change
-    create_table :alchemy_page_mutexes do |t|
-      t.references :page, null: false, type: "int(11)",index: {unique: true}, foreign_key: {to_table: :alchemy_pages}
-      t.datetime :created_at
+    unless table_exists?(:alchemy_page_mutexes)
+      create_table :alchemy_page_mutexes do |t|
+        t.references :page, null: false, type: "int(11)", index: {unique: true}, foreign_key: {to_table: :alchemy_pages}
+        t.datetime :created_at
+      end
     end
   end
 end
